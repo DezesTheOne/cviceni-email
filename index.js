335b6f87dc4b69f7ecf6422721182860c70f5e63
DezesTheOne <hedvika.reichlova@gmail.com>
2023-09-25 19:42:12 +0200

    hotovo

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('funguju');
+const goodbye = (name = '') => {
+  const closingParagraph = document.querySelector('.email__closing');
+  closingParagraph.textContent = `S pozdravem ${name}`;
+};
+
+goodbye('Pavel Ovesný');
+
